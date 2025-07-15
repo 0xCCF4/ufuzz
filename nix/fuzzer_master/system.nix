c280772fed9bd9d568380f1a198ff229f997e3e8
0xCCF4 <0xccf4-git-commit@lenzentech.de>
2025-07-15 15:16:31 +0000

    added README for nix folder and cleanup config files

--- nix/fuzzer_master/system.nix
+++ nix/fuzzer_master/system.nix
@@ -8,89 +8,13 @@
       networking.interfaces.eth0 = {
         ipv4.addresses = [
           {
-            address = "10.83.3.250";
+            address = "10.83.3.250"; # change the network configuration for your project
             prefixLength = 24;
           }
         ];
       };
-      networking.defaultGateway = "10.83.3.1";
+      networking.defaultGateway = "10.83.3.1"; # change this
 
-      services.journald.extraConfig = "SystemMaxUse=64G";
-
-      systemd.services.fuzzer_master = {
-        description = "Fuzzer Master Service";
-        after = [ "network.target" ];
-        wantedBy = [ "multi-user.target" ];
-        conflicts = [ "fuzzer_master_corpus.service" "spec_fuzz.service" "fuzzer_afl.service" ];
-
-        enable = false;
-
-        serviceConfig = {
-          ExecStart = "${packages."${system}".fuzzer_master}/bin/fuzz_master genetic";
-          WorkingDirectory = "/home/thesis";
-          Restart = "always";
-          RestartSec = 5;
-        };
-        environment = {
-          RUST_LOG = "trace";
-        };
-      };
-
-      systemd.services.fuzzer_master_corpus = {
-        description = "Fuzzer Master Service with corpus";
-        after = [ "network.target" ];
-        wantedBy = [ "multi-user.target" ];
-        conflicts = [ "fuzzer_master.service" "spec_fuzz.service" "fuzzer_afl.service" ];
-
-        enable = false;
-
-        serviceConfig = {
-          ExecStart = "${packages."${system}".fuzzer_master}/bin/fuzz_master genetic /home/thesis/corpus.json";
-          WorkingDirectory = "/home/thesis";
-          Restart = "always";
-          RestartSec = 5;
-        };
-        environment = {
-          RUST_LOG = "trace";
-        };
-      };
-
-      systemd.services.spec_fuzz = {
-        description = "Spec Fuzz Service";
-        after = [ "network.target" ];
-        wantedBy = [ "multi-user.target" ];
-        conflicts = [ "fuzzer_master.service" "fuzzer_master_corpus.service" "fuzzer_afl.service" ];
-
-        enable = false;
-
-        serviceConfig = {
-          ExecStart = "${packages."${system}".fuzzer_master}/bin/fuzz_master spec /home/thesis/spec_fuzz.json";
-          WorkingDirectory = "/home/thesis";
-          Restart = "always";
-          RestartSec = 5;
-        };
-        environment = {
-          RUST_LOG = "trace";
-        };
-      };
-
-      systemd.services.fuzzer_afl = {
-        description = "AFL Fuzzer Service";
-        after = [ "network.target" ];
-        wantedBy = [ "multi-user.target" ];
-        conflicts = [ "fuzzer_master.service" "fuzzer_master_corpus.service" "spec_fuzz.service" ];
-
-        enable = false;
-
-        serviceConfig = {
-          ExecStart = "${packages."${system}".fuzzer_master}/bin/fuzz_master afl -s /home/thesis/afl_solutions/";
-          WorkingDirectory = "/home/thesis";
-          Restart = "always";
-          RestartSec = 5;
-        };
-        environment = {
-          RUST_LOG = "trace";
-        };
-      };
+      services.journald.extraConfig = "SystemMaxUse=64G"; # if you use a smaller SD card feel free to lower the max storage space
     };
 }
--- nix/fuzzer_node/system.nix
+++ nix/fuzzer_node/system.nix
@@ -14,14 +14,14 @@
       networking.interfaces.eth0 = {
         ipv4.addresses = [
           {
-            address = "10.83.3.198";
+            address = "10.83.3.198"; # change the network configuration for your project
             prefixLength = 24;
           }
         ];
       };
-      networking.defaultGateway = "10.83.3.1";
+      networking.defaultGateway = "10.83.3.1"; # change this
 
-      services.journald.extraConfig = "SystemMaxUse=8G";
+      services.journald.extraConfig = "SystemMaxUse=8G"; # if you use a smaller SD card feel free to lower the max storage space
 
       systemd.services.fuzzer_node = {
         description = "Fuzzer Node Service";
